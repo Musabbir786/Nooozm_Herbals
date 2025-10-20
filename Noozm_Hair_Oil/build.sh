6caf0814a6ba18fe249329c720194cccea69e3fb
musabbir <150608597+Musabbir786@users.noreply.github.com>
2025-10-20 15:45:10 +0530

    Update build.sh

--- Noozm_Hair_Oil/build.sh
+++ Noozm_Hair_Oil/build.sh
@@ -1,1 +1,8 @@
-﻿#!/usr/bin/env bash# exit on errorset -o errexitpip install -r requirements.txtpython manage.py collectstatic --no-inputpython manage.py migrate
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
+python manage.py migrate
